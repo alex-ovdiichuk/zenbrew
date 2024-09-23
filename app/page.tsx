0c14d30842580f6b130b7022f022a935adf4f790
alex-ovdiichuk <alex.yokai00@gmail.com>
2024-09-23 11:34:13 +0300

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Hero } from "@/components/Hero";
+
+const HomePage = () => {
+  return (
+    <div className="h-full overflow-x-hidden">
+      <Hero />
+    </div>
+  );
+};
+
+export default HomePage;
